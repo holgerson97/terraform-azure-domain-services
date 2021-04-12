cc1376eb9009a9b7933f3f193fc3356a8c9dbbdd
Nils Carstensen <nilscarstensen@live.com>
2021-04-12 17:30:37 +0200

    Initial commit

---
+++ examples/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.42.0"
+    }
+  }
+}
+
+provider "azurerm" {
+
+    client_id       = "xxxxxxxxxxxxxxxxxxxxxxx"
+    client_secret   = "xxxxxxxxxxxxxxxxxxxxxxx"
+    subscription_id = "xxxxxxxxxxxxxxxxxxxxxxx"
+    tenant_id       = "xxxxxxxxxxxxxxxxxxxxxxx"
+
+}
+
+resource "azurerm_resource_group" "example" {
+
+  name     = "example"
+  location = "West Europe"
+  
+}
+
+resource "azurerm_virtual_network" "example" {
+
+  name                = "virtualNetwork1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+
+}
+
+resource "azurerm_subnet" "example" {
+
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+  
+}
+
+module "azure_adds" {
+
+    source  = "github.com/holgerson97/terraform-azure-domain-services//module"
+
+    rg_name     = azurerm_resource_group.main.name
+    location    = "West Europe"
+    subnet-name = azurerm_subnet.example.name
+    subnet-id   = azurerm_subnet.example.id
+    vnet-name   = azurerm_virtual_network.example.name
+
+}
